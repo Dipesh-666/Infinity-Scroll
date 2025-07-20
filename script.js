9667a0fff668ceab201c1b1122fa1356213cc0a5
Dipesh-666 <dipeshsilwal666@gmail.com>
2025-07-20 14:00:57 +0545

    loader added

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 const imageContainer = document.getElementById('imageContainer');
 const loader = document.getElementById('loader');
 let photoArray = [];
-const count = 10;
-const url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${count}`;//key is in another js file untraced bt git
+const count = 3;
+let imagesLoaded = 0;
+let ready = false;
+let totalImages = 0;
+
+const url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${count}`;//key is in another js file untraced by git
+function imageLoaded()
+{
+    imagesLoaded++;
+    if(imagesLoaded === totalImages)
+    {
+        ready = true;
+        loader.hidden = true;
+        
+        
+    }
+    console.log('image loaded');
+}
 function displayImage(){
+    imagesLoaded = 0;
+    totalImages = photoArray.length;
+   
     photoArray.forEach((photo) => {
         const anchor = document.createElement('a');
         anchor.setAttribute('href', photo.links.html);
@@ -13,9 +32,12 @@ function displayImage(){
         image.setAttribute('src',photo.urls.regular);
         image.setAttribute('alt',photo.alt_description);
         image.setAttribute('title',photo.alt_description);
+        image.addEventListener('load', imageLoaded);
         anchor.appendChild(image);
         imageContainer.appendChild(anchor);  
     });
+    
+
 };
 async function getImage()
 {
@@ -26,18 +48,21 @@ async function getImage()
             throw new Error("Unable to fetch from API."); 
         }
         photoArray = await response.json();
-        console.log(photoArray);
+        
         displayImage();
     } catch (error) {
-    // console.error(error);      
+        
     }
     
 }
 
 getImage();
 window.addEventListener('scroll', () =>{
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000)
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)
     {
+        ready = false;
         getImage();
         }
 })
+
+
